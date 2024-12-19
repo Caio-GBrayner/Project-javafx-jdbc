4af28648c7b6e8cac6cfb635f6e7e52fb721e11f
Caio Gomes Brayner <caiogomesbrayner@gmail.com>
2024-12-19 13:20:38 -0300

    added mysql logic from previous project, updated module-info, dao.findAll().

--- src/module-info.java
+++ src/module-info.java
@@ -2,11 +2,15 @@ module workshop_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	
+	requires transitive java.sql;
+	
 	opens gui to javafx.fxml;
 	exports gui;
 	
 	exports model.services;
 	exports model.entities;
+	exports model.dao;
+	exports model.dao.impl;
 
 	opens application to javafx.graphics, javafx.fxml;
 }
